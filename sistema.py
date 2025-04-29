8ba5df082429d372257032cbee3cfd8fc334c0a7
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-04-29 09:36:06 -0300

    esse codigo faz a soma de dois numeros e mutiplica o resultado dessa soma por um terceiro numero

---
+++ sistema.py
@@ -0,0 +1,13 @@
+numero01 = int(input("Digite um numero: "))
+numero02 = int(input("Digite um numero: "))
+
+soma = numero01 + numero02 
+
+print(soma)
+
+numero03 = int(input("Digite mais um nomero: "))
+
+
+mutiplicacao = soma * numero03
+
+print(f"A mutiplicaçao do resultado da soma com o terceiro numero é: {mutiplicacao}")
