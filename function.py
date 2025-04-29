f741ad83a43acd0e589f470a5d483d0285b158f3
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-04-29 12:12:36 -0300

    aprendemos a usar o def

---
+++ function.py
@@ -0,0 +1,31 @@
+def soma():
+    num_01 = 25
+    num_02 = 31
+
+    resultado = num_01 + num_02
+
+    return resultado
+
+def somarDoisNumeros (nota_01, nota_02):
+    resultado = nota_01 + nota_02
+    
+    return print(resultado)
+
+# somarDoisNumeros(24, 98)
+
+num_03 = 12
+num_04 = 9
+
+def multiplicarDoisNumeros (nota03, nota_04):
+    resultado = num_03 * num_04
+
+    return print(resultado)
+
+def cadastrar (nome, idade):
+    data = {
+        "nome" : nome, 
+        "idade" : idade
+    }
+    return print(data)
+
+cadastrar("Nicholas", 16)
